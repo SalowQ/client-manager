e8e73a787c1b562bb8957d48d5189e5f5b9828a2
Igor Sousa <arqurb.igor@gmail.com>
2025-07-06 12:39:12 -0300

    feat: configure react-query in clients

--- apps/clients/src/App.tsx
+++ apps/clients/src/App.tsx
@@ -1,20 +1,24 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import ClientsLayout from "./components/ClientsLayout";
+import { queryClient } from "./lib/react-query";
 import ListClients from "./pages/ListClients";
 import ListSelectedClients from "./pages/ListSelectedClients";
 
 function App() {
   return (
-    <ClientsLayout>
-      <Routes>
-        <Route path="list" element={<ListClients />} />
-        <Route path="list-selected" element={<ListSelectedClients />} />
-        <Route path="create" element={<h2>Criar Cliente</h2>} />
-        <Route path="/" element={<h1>clients</h1>} />
-        <Route path="*" element={<Navigate to="/404" replace />} />
-      </Routes>
-    </ClientsLayout>
+    <QueryClientProvider client={queryClient}>
+      <ClientsLayout>
+        <Routes>
+          <Route path="list" element={<ListClients />} />
+          <Route path="list-selected" element={<ListSelectedClients />} />
+          <Route path="create" element={<h2>Criar Cliente</h2>} />
+          <Route path="/" element={<h1>clients</h1>} />
+          <Route path="*" element={<Navigate to="/404" replace />} />
+        </Routes>
+      </ClientsLayout>
+    </QueryClientProvider>
   );
 }
 
