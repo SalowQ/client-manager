429be20347e9d2a5c6f43103f01a4527c84e6e08
Igor Sousa <112203229+SalowQ@users.noreply.github.com>
2025-07-07 13:52:19 -0300

    test: add new test for localStorage functionality in Login component

--- apps/auth/src/pages/Login.spec.tsx
+++ apps/auth/src/pages/Login.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { describe, expect, vi } from "vitest";
 import Login from "./Login";
@@ -53,5 +53,32 @@ describe("Login", () => {
 
     expect(screen.getByTestId("login-button")).toBeInTheDocument();
     expect(screen.getByText("Entrar")).toBeInTheDocument();
+  });
+
+  test("salva o nome no localStorage quando o formulário é submetido", async () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
+
+    const input = screen.getByTestId("input");
+    const submitButton = screen.getByTestId("login-button");
+
+    fireEvent.change(input, { target: { value: "João Silva" } });
+
+    fireEvent.click(submitButton);
+
+    await waitFor(
+      () => {
+        expect(localStorageMock.setItem).toHaveBeenCalledWith(
+          "userName",
+          "João Silva"
+        );
+      },
+      { timeout: 2000 }
+    );
+
+    expect(mockNavigate).toHaveBeenCalledWith("/clients/list");
   });
 });
