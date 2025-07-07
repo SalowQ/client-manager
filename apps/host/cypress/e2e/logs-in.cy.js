3b895cb245a5a3b33046300d1a14fd4d7c9275c7
Igor Sousa <112203229+SalowQ@users.noreply.github.com>
2025-07-07 16:53:14 -0300

    feat: install and configure cypress

---
+++ apps/host/cypress/e2e/logs-in.cy.js
@@ -0,0 +1,11 @@
+describe("loga no sistema inserindo um nome", () => {
+  it("deve logar no sistema inserindo um nome", () => {
+    cy.visit("/");
+
+    // cy.get('input[name="name"]').type("João Silva");
+
+    // cy.get('button[type="submit"]').click();
+
+    // cy.url().should("include", "/clients/list");
+  });
+});
